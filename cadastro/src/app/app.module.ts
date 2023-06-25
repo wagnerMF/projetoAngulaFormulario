c348c06165a1e6d0395348c84066d80688b3c9af
Wagner Rodrigues <gerencia988@gmail.com>
2023-06-24 23:22:48 -0300

    estrutura inicial

---
+++ cadastro/src/app/app.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { CadastroComponent } from './cadastro/cadastro.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CadastroComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+
+})
+export class AppModule { }
---
+++ cadastro/src/app/cadastro/cadastro.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cadastro',
+  templateUrl: './cadastro.component.html',
+  styleUrls: ['./cadastro.component.css']
+})
+export class CadastroComponent {
+
+
+}
